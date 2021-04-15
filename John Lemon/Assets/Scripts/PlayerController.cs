994c88947f22723e9fbae294c542c6d43695b447
Luis Ortiz <nachortiznajera@gmail.com>
2021-04-15 03:55:02 -0500

    Movimiento jugador
    Implementación del movimiento del jugador con el teclado

--- John Lemon/Assets/Scripts/PlayerController.cs
+++ John Lemon/Assets/Scripts/PlayerController.cs
@@ -1,14 +1,43 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
+    [SerializeField]
+    private float turnSpeed;
+    private const string IS_WALKING = "IsWalking";
     
+    private Vector3 movement;
+    private Quaternion rotation = Quaternion.identity;
+
+    private Rigidbody _rigidbody;
+    private Animator _animator;
+
     void Start() {
-        
+        _rigidbody = GetComponent<Rigidbody>();
+        _animator = GetComponent<Animator>();
     }
 
     void Update() {
+        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
         
+        movement.Set(horizontalInput, 0, verticalInput);
+        movement.Normalize();
+        
+        bool hasVerticalInput = !Mathf.Approximately(verticalInput, 0);
+        bool hasHorizontalInput = !Mathf.Approximately(horizontalInput, 0);
+        bool isWalking = hasVerticalInput || hasHorizontalInput;
+        
+        _animator.SetBool(IS_WALKING, isWalking);
+        Vector3 desiredFoward = Vector3.RotateTowards(transform.forward, movement,
+            turnSpeed * Time.deltaTime, 0);
+        rotation = Quaternion.LookRotation(desiredFoward);
+    }
+
+    private void OnAnimatorMove() {
+        _rigidbody.MovePosition(_rigidbody.position + movement * _animator.deltaPosition.magnitude);
+        _rigidbody.MoveRotation(rotation);
     }
 }
